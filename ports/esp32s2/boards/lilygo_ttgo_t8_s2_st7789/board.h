2077233ecd13c978780f3fd630e86c0dbddbb247
Bruce Segal <bsegal@skieast.net>
2021-02-02 17:45:14 -0800

    Working TTGO T8 ST7789 port

--- ports/esp32s2/boards/lilygo_ttgo_t8_s2_st7789/board.h
+++ ports/esp32s2/boards/lilygo_ttgo_t8_s2_st7789/board.h
@@ -35,34 +35,40 @@
 //--------------------------------------------------------------------+
 // TFT Display ST7789
 //--------------------------------------------------------------------+
-#undef  CONFIG_LCD_TYPE_AUTO
+// #undef  CONFIG_LCD_TYPE_AUTO
 #define CONFIG_LCD_TYPE_ST7789V
 
-#define PIN_DISPLAY_MISO    -1              //No MISO connected to display.
-#define PIN_DISPLAY_MOSI    35
-#define PIN_DISPLAY_SCK     36
-#define PIN_DISPLAY_CS      34
-#define PIN_DISPLAY_DC      37
-#define PIN_DISPLAY_RST     38
-#define PIN_DISPLAY_BL      33
+#define DISPLAY_PIN_MISO    -1              //No MISO connected to display.
+#define DISPLAY_PIN_MOSI    35
+#define DISPLAY_PIN_SCK     36
+#define DISPLAY_PIN_CS      34
+#define DISPLAY_PIN_DC      37
+#define DISPLAY_PIN_RST     38
+#define DISPLAY_PIN_BL      33
+#define DISPLAY_BL_ON       14
 #define PIN_POWER           14
 
 #define DISPLAY_BL_STATE        1  // GPIO state to enable back light
 #define DISPLAY_WIDTH           240
 #define DISPLAY_HEIGHT          135
-#define DISPLAY_MADCTL          0x36     //Display Memory Access Control ST7789
+#define DISPLAY_MADCTL   (TFT_MADCTL_MX | TFT_MADCTL_RGB)
+// #define DISPLAY_MADCTL          (0x36)     //Display Memory Access Control ST7789
 // #define DISPLAY_MADCTL          (TFT_MADCTL_MX)     //Display Memory Access Control ST7789
 #define DISPLAY_ROTATION        1
+#define DISPLAY_COL_OFFSET    53
+#define DISPLAY_ROW_OFFSET    40
+
+#define DISPLAY_TITLE        "TTGO S2DISP"
 //--------------------------------------------------------------------+
 // USB UF2
 //--------------------------------------------------------------------+
 
 #define USB_VID           0x303A            // Espressif VID
 #define USB_PID           0x8008            // Espressif assigned PID
-#define USB_MANUFACTURER  "Espressif"
-#define USB_PRODUCT       "TTGO_T8_S2_ST7789"
+#define USB_MANUFACTURER  "LILYGO"
+#define USB_PRODUCT       "TTGO_T8_S2_Display"
 
 #define UF2_PRODUCT_NAME  USB_MANUFACTURER " " USB_PRODUCT
-#define UF2_BOARD_ID      "LilyGO-TTGO-T8-S2-ST7789"
+#define UF2_BOARD_ID      "LilyGO-TTGO-T8-S2-DISPLAY"
 #define UF2_VOLUME_LABEL  "TTGOS2BOOT"
 #define UF2_INDEX_URL     "http://www.lilygo.cn/prod_view.aspx?TypeId=50033&Id=1321&FId=t3:50033:3"
